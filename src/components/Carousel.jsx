46f4a30dcb632d2e2a6908907b3690942a29391d
Irsannerha <irsanrh275@gmail.com>
2023-11-23 19:35:46 +0700

    PWL - ALTNEX CONSOLE

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,8 +4,10 @@ import CarouselImage from "./CarouselImage";
 
 import ImageBgPlay1 from "../assets/img/bgPlay1.png";
 import ImageBgPlay2 from "../assets/img/bgPlay2.png";
-import ImageBgPlay3 from "../assets/img/bgPlay3.jpg";
+import ImageBgPlay3 from "../assets/img/bgPlay3.png";
 import ImageBgPlay4 from "../assets/img/bgPlay4.png";
+import ImageBgPlay5 from "../assets/img/bgPlay5.png";
+import ImageBgPlay6 from "../assets/img/bgPlay6.png";
 
 function CarouselItem() {
   return (
@@ -13,36 +15,54 @@ function CarouselItem() {
       <Carousel.Item>
         <CarouselImage text="First slide" image={ImageBgPlay1} />
         <Carousel.Caption>
-          {/* <h3>First slide label</h3> */}
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <CarouselImage text="Second slide" image={ImageBgPlay2} />
         <Carousel.Caption>
-          {/* <h3>Second slide label</h3> */}
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <CarouselImage text="Third slide" image={ImageBgPlay3} />
         <Carousel.Caption>
-          {/* <h3>Third slide label</h3> */}
+          {/* <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <CarouselImage text="Fourd slide" image={ImageBgPlay4} />
         <Carousel.Caption>
-          {/* <h3>Fourd slide label</h3> */}
+          {/* <h3>Fourd slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CarouselImage text="Fourd slide" image={ImageBgPlay5} />
+        <Carousel.Caption>
+          {/* <h3>Fourd slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <CarouselImage text="Fourd slide" image={ImageBgPlay6} />
+        <Carousel.Caption>
+          {/* <h3>Fourd slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
